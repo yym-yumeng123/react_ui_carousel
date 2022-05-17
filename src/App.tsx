4362052a4f447b02587482579df95c9ad6b15b4f
yangyumeng <yangyumeng@guanmai.cn>
2022-05-17 16:55:30 +0800

    给 button 添加一个默认样式 和 一个 type 属性

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Button } from "./lib/index";
+import "./App.scss";
 
 const App = () => {
   return (
     <div className="App">
       <Button />
+      <Button type="primary" />
+      <Button type="danger" />
     </div>
   );
 };
--- src/lib/Button/button.tsx
+++ src/lib/Button/button.tsx
@@ -1,9 +1,21 @@
-import { FC } from "react";
+import { ButtonHTMLAttributes, FC } from "react";
+import classnames from "classnames";
+import "./button.scss";
 
-interface ButtonProps {}
+interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLElement>, "type"> {
+  type?: "primary" | "danger" | "default";
+}
 
 const Button: FC<ButtonProps> = (props) => {
-  return <div>Button</div>;
+  const { type = "default", ...restProps } = props;
+  const cn = {
+    [`g-button-${type}`]: type,
+  };
+  return (
+    <button className={classnames("g-button", cn)} {...restProps}>
+      按钮
+    </button>
+  );
 };
 
 export default Button;
